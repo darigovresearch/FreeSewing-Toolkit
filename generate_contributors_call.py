fe1ce14479cb7f12279f06b9f103778640491144
Darigov Research <30328618+darigovresearch@users.noreply.github.com>
2022-02-19 11:02:38 +0000

    feat: Adds autogenerated time zones to generate_contributors_call.py

--- generate_contributors_call.py
+++ generate_contributors_call.py
@@ -1,34 +1,51 @@
 import os
 
 # inputing date from command line
-date = input("What date is the call? ")
+date = input("What date is the call? (i.e. 19 February 2022) ")
 print("Date chosen: " + date)
 
+# Getting relevant date parameters for time zone link generation
+date_split = date.split(" ")
+day = date_split[0]
+month = date_split[1]
+year = date_split[2]
+
 # inputing time from command line
-time = input("What time is the call? (leave blank to set to 16:00 UTC)")
+time = input("What time is the call in UTC? (leave blank to set to 4pm UTC) ")
 print(time)
+
+# savvy time link based off of previous time zones in community call notes
+link_base = "https://savvytime.com/converter/utc-to-ca-los-angeles-il-chicago-ny-new-york-city-brazil-sao-paulo-united-kingdom-london-france-paris-south-africa-johannesburg-msd-india-mumbai-hkt-japan-tokyo-australia-sydney/"
 
 # code to handle default time if left blank
 if time == "":
-	# set the default time if input is set to blank
-	# TODO autogenerate time zones
-	time = """16:00 UTC, which is:
-Los Angeles: 09:00
-Chicago: 11:00
-New York: 12:00
-São Paulo: 14:00
-London: 17:00
-Paris: 18:00
-Johannesburg: 19:00
-Moscow: 20:00
-Mumbai: 22:30
-Hong Kong: 01:00 on Sunday
-Tokyo: 02:00 on Sunday
-Sydney: 04:00 on Sunday"""
-	print("Default time chosen: 16:00 UTC")
+    # set the default time if input is set to blank
+    # autogenerate time zones for default time
+    link = link_base + month[0:3] + "-" + day + "-" + year + "/" + "4pm"
+    time = "4pm UTC, which is can be found in your local time zone [here](%s)" % link
+
+    #     time = """4pm UTC, which is:
+    # Los Angeles: 09:00
+    # Chicago: 11:00
+    # New York: 12:00
+    # São Paulo: 14:00
+    # London: 17:00
+    # Paris: 18:00
+    # Johannesburg: 19:00
+    # Moscow: 20:00
+    # Mumbai: 22:30
+    # Hong Kong: 01:00 on Sunday
+    # Tokyo: 02:00 on Sunday
+    # Sydney: 04:00 on Sunday"""
+    print("Default time chosen: 4pm UTC")
 
 else:
-	print("Time chosen: " +  time)
+    # autogenerate time zones for given time
+    temp_time = time
+    link = link_base + month[0:3] + "-" + day + "-" + year + "/" + temp_time
+
+    time = """%s UTC, which is can be found in your local time zone [here](%s)""" % (temp_time, link)
+    print("Time chosen: " +  time)
 
 # inputing hosts from command line
 hosts = input("Who are the hosts? (leave blank to set to @tangerineshark and @lucibytes)")
@@ -36,12 +53,12 @@ print(hosts)
 
 # code to handle default hosts if left blank
 if hosts == "":
-	# set the default hosts if input is set to blank
-	hosts = """@tangerineshark and @lucibytes"""
-	print("Default hosts chosen: @tangerineshark and @lucibytes")
+    # set the default hosts if input is set to blank
+    hosts = """@tangerineshark and @lucibytes"""
+    print("Default hosts chosen: @tangerineshark and @lucibytes")
 
 else:
-	print("hosts chosen: " +  time)
+    print("hosts chosen: " +  time)
 
 
 # generating full string to copy & paste into form
